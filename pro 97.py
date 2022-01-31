9bb10d1d0d5f26695224d2a46fd8068977175804
mautik20 <73740190+mautik20@users.noreply.github.com>
2022-01-31 10:55:03 +0530

    Add files via upload

---
+++ pro 97.py
@@ -0,0 +1,10 @@
+Number guessing game
+Guess a number (between 1 and 9):
+Enter your guess :- 3
+Your guess was to low :- Guess a number higher than 3
+Enter your guess :- 4
+Your guess was to low :- Guess a number higher than 4
+Enter your guess :- 7
+Your guess was to low :- Guess a number higher than 7
+Enter your guess :- 9
+Congratulation YOU WON
